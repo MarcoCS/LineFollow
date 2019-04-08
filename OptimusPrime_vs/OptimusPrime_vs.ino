4bfd076a40daa037fcbeeea4dbf0927065b5c6d4
MarcoCS <43823775+MarcoCS@users.noreply.github.com>
2019-04-08 15:27:42 -0600

    updating w/ latest build

--- OptimusPrime_vs/OptimusPrime_vs.ino
+++ OptimusPrime_vs/OptimusPrime_vs.ino
@@ -4,11 +4,22 @@
 // ----------------------------------------
 #include <Sabertooth.h>
 
-int leftIR = 0;
+int leftIR = 2;
 int centerIR = 1;
-int rightIR = 2;
-int black = 400; // To be determined
+int rightIR = 0;
 
+// Different types of black
+int BlackL = 640;
+int BlackC = 640;
+int BlackR = 640;
+
+// SensorVal
+int left;
+int right;
+int center;
+
+// Directive
+int dir; // 0 = forward, 1 = left, 2 = right
 
 // Motor stuff
 Sabertooth Drive(128);
@@ -19,34 +30,48 @@ void setup() {
   pinMode(rightIR, INPUT);
   SabertoothTXPinSerial.begin(38400);
   Drive.autobaud();
-  Serial.begin(9600);
-
+  //Serial.begin(9600);
 }
 
 void loop() {
+  /*
   Serial.print("left:");
   Serial.println(analogRead(leftIR));
   Serial.print("center:");
   Serial.println(analogRead(centerIR));
   Serial.print("right:");
   Serial.println(analogRead(rightIR));
-  
-  if (analogRead(centerIR) >= black) {
-    Drive.motor(1, 32);
-    Drive.motor(2, 32);
-  }
-  if (analogRead(leftIR) >= black) {
-    Drive.motor(1, 32);
-    Drive.motor(2, 20);
-  }
-  if (analogRead(rightIR) >= black) {
-    Drive.motor(1, 20);
-    Drive.motor(2, 32);
-  }
+  */
 
-  if (analogRead(centerIR) and analogRead(leftIR) and analogRead(rightIR)) {
-    Drive.motor(1, -20);
-    Drive.motor(2, -20);
+  
+  right = analogRead(rightIR);
+  left = analogRead(leftIR);
+  center = analogRead(centerIR);
+  // 0 = fwd, 1 = left, 2 = right
+  if (center > right and center > left) {
+    dir = 0;
+  }
+  if (right > center and right > left) {
+    dir = 2;
+  }
+  if (left > center and left > right) {
+    dir = 1;
+  }
+  
+  if (dir == 0) {
+    //Serial.println("Center");
+    Drive.motor(1, 40);
+    Drive.motor(2, 40);
+  }
+  if (dir == 1) {
+    //Serial.println("Left");
+    Drive.motor(1, 40);
+    Drive.motor(2, -40);
+  }
+  if (dir == 2) {
+    //Serial.println("Right");
+    Drive.motor(1, -40);
+    Drive.motor(2, 40);
   }
 
 }
